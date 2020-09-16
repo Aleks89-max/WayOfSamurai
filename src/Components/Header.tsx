c6140938ceb172c506306246dd582283357c1725
Greisvander <pestryakov_899@mail.ru>
2020-09-16 07:43:22 +0300

    Lesson 12 breakdown into components

---
+++ src/Components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css'
+function Header() {
+    return<header className='header'>
+            <img src="https://i.pinimg.com/736x/70/99/3c/70993c579d5e336da07f558f7b842b11--clean-energy-logo-energy-graphic.jpg" alt="beach"/>
+        </header>
+
+
+}
+export default Header
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import '../App.css'
+function Profile() {
+return <div className='content'>
+        <div>
+            <img src="https://www.sunhome.ru/i/wallpapers/51/sklon-gori-v2.orig.jpg" alt="beach"/>
+        </div>
+        <div>Ava + Description</div>
+        <div>
+            My posts
+            <div>New post</div>
+            <div>
+                <div>post1</div>
+                <div>post2</div>
+                <div>post3</div>
+                <div>post4</div>
+            </div>
+
+        </div>
+
+    </div>
+
+}
+export default Profile
