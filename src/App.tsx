2c2c69ff2717835ed1cdc1214d41d30b0ed714b5
Greisvander <pestryakov_899@mail.ru>
2020-09-16 07:16:07 +0300

    Lesson 12 breakdown into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,15 @@
 import React from 'react';
-
 import './App.css';
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+import Profile from "./Components/Profile";
 
 function App() {
   return (
     <div className="app-wrapper">
-     <header className='header'>
-         <img src="https://i.pinimg.com/736x/70/99/3c/70993c579d5e336da07f558f7b842b11--clean-energy-logo-energy-graphic.jpg" alt="beach"/>
-     </header>
-        <nav className='nav'>
-            <div>
-                <a>Profile</a>
-            </div>
-            <div>
-                <a> Messages</a>
-            </div>
-            <div>
-                <a> News</a>
-            </div>
-            <div>
-                <a> Music</a>
-            </div>
-            <div>
-                <a>Settings</a>
-            </div>
-        </nav>
-        <div className='content'>
-            <div>
-            <img src="https://www.sunhome.ru/i/wallpapers/51/sklon-gori-v2.orig.jpg" alt="beach"/>
-            </div>
-            <div>Ava + Description</div>
-            <div>
-                My posts
-            <div>New post</div>
-                <div>
-                    <div>post1</div>
-                    <div>post2</div>
-                    <div>post3</div>
-                    <div>post4</div>
-                </div>
-
-            </div>
-
-        </div>
+       <Header/>
+       <Navbar/>
+       <Profile/>
     </div>
   );
 }
